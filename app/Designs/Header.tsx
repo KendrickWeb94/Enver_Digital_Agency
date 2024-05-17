80036db5678e4c8bf9626ca27496ae2bee8373eb
Kendrick Victor <sunflowerroses801@gmail.com>
2024-05-17 15:25:01 +0000

    Update Header.tsx

--- app/Designs/Header.tsx
+++ app/Designs/Header.tsx
@@ -1,6 +1,8 @@
+"use client"
+
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import logolight from "../assets/logo.png";
 import logodark from "../assets/logo-dark.png";
 import NavItems from "./NavItem";
@@ -9,32 +11,53 @@ import { Button } from "@/components/ui/button";
 import ResponsiveNavbar from "./ResponsiveNavbar";
 
 const Header = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-   <div className=" top-0 fixed bg-white z-50  dark:bg-black w-full">
-     <div className=" w-full z-40 relative mx-auto py-3 max-w-[95%]">
-      <div className=" flex items-center justify-between w-full">
-        <div className="">
-          <Link href="/" className="hidden dark:block ">
-            <Image src={logolight} alt="logo" className=" w-28" />
-          </Link>
-          <Link href="/" className=" dark:hidden   ">
-            <Image src={logodark} alt="logo" className=" w-28" />
-          </Link>
-        </div>
-        <div className=" md:flex ds:hidden flex items-center gap-7 ">
-          <NavItems links={navLinks} />
-        </div>
-        <div className="">
-          <Button className=" bg-transparent border dark:border-white border-black/75 dark:text-white text-black hover:text-white dark:hover:text-black">
-            Get Started
-          </Button>
-        </div>
-        <div className=" md:hidden ds:block">
-          <ResponsiveNavbar />
+    <div
+      className={`top-0 fixed z-50 w-full transition-colors delay-500 ease-in-out duration-500 ${
+        isScrolled ? "bg-white dark:bg-black" : "bg-transparent"
+      }`}
+    >
+      <div className="w-full z-40 relative mx-auto py-3 max-w-[95%]">
+        <div className="flex items-center justify-between w-full">
+          <div className="">
+            <Link href="/" className="hidden dark:block">
+              <Image src={logolight} alt="logo" className="w-28" />
+            </Link>
+            <Link href="/" className="dark:hidden">
+              <Image src={logodark} alt="logo" className="w-28" />
+            </Link>
+          </div>
+          <div className="md:flex ds:hidden flex items-center gap-7">
+            <NavItems links={navLinks} />
+          </div>
+          <div className="">
+            <Button className="bg-transparent border dark:border-white border-black/75 dark:text-white text-black hover:text-white dark:hover:text-black">
+              Get Started
+            </Button>
+          </div>
+          <div className="md:hidden ds:block">
+            <ResponsiveNavbar />
+          </div>
         </div>
       </div>
     </div>
-   </div>
   );
 };
 
