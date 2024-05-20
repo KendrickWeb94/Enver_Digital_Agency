d6e2836f0d47755356adb72abb7a70c68f2ca28d
Kendrick Victor <sunflowerroses801@gmail.com>
2024-05-20 16:34:06 +0100

    Add files via upload

--- app/Main.tsx
+++ app/Main.tsx
@@ -8,45 +8,25 @@ import { motion } from "framer-motion";
 const Main = () => {
   return (
    <div className="flex flex-col gap-[10rem]">
-     <motion.div
-      initial={{ opacity: 0, x: 10 }}
-      whileInView={{ opacity: 1, x: 0 }}
-      transition={{ ease: "easeInOut", delay: 1, duration: 2 }}
-    >
+    
       <div className=" w-full  o relative z-40 pt-4  mx-auto max-w-[95%]">
         <Services />
 
-        <motion.div
-          initial={{ opacity: 0, x: 0 }}
-          whileInView={{ opacity: 1, x: 0 }}
-          transition={{ ease: "easeInOut", delay: 1, duration: 2 }}
-        >
+       
           <div className=" w-full  o relative z-40 pt-4  mx-auto max-w-[95%]">
             <Portfolio />
           </div>
-        </motion.div>
-
-        <motion.div
-          initial={{ opacity: 0, x: -50 }}
-          whileInView={{ opacity: 1, x: 0 }}
-          transition={{ ease: "easeInOut", delay: 1, duration: 2 }}
-        >
+     
           <div className=" w-full  o relative z-40 pt-4  mx-auto max-w-[95%]">
             <Testimonials />
           </div>
-        </motion.div>
-
-        <motion.div
-          initial={{ opacity: 0, x: 100 }}
-          whileInView={{ opacity: 1, x: 0 }}
-          transition={{ ease: "easeInOut", delay: 1, duration: 2 }}
-        >
+      
           <div className=" w-full  o relative z-40 pt-4  mx-auto max-w-[95%]">
             <AccordionDemo />
           </div>
-        </motion.div>
+       
       </div>
-    </motion.div>
+  
    </div>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -28,67 +28,35 @@ const Page = () => {
           <Hero />
         </div>
       </motion.div>
-      <motion.div
-        initial={{ opacity: 0, y: 30 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        transition={{ ease: "easeInOut", delay: 1, duration: 2 }}
-      >
+    
         <div className=" dark:bg-slate-950 bg-slate-200">
           <WhyChooseUs />
         </div>
-      </motion.div>
+    
 
       <Main />
-      <motion.div
-        initial={{ opacity: 0, x: -100 }}
-        whileInView={{ opacity: 1, x: 0 }}
-        transition={{ ease: "easeInOut", delay: 1, duration: 2 }}
-      >
+      
         <div className="bg dark:bg-zinc-900 flex-center flex-col space-y-8 p-5 w-full min-h-screen mx-auto ">
           <DataTableDemo />
         </div>
-      </motion.div>
-      <motion.div
-        initial={{ opacity: 0, x: -700 }}
-        whileInView={{ opacity: 1, x: 0 }}
-        transition={{ ease: "easeInOut", delay: 1, duration: 2 }}
-      ></motion.div>
-      <motion.div
-        initial={{ opacity: 0, x: 300 }}
-        whileInView={{ opacity: 1, x: 0 }}
-        transition={{ ease: "easeInOut", delay: 1, duration: 2 }}
-      >
+      
+     
         <div className="bg  flex-center flex-col space-y-8 p-5 w-full min-h-screen mx-auto ">
           <Payment />
         </div>
-      </motion.div>
-      <motion.div
-        initial={{ opacity: 0, x: -300 }}
-        whileInView={{ opacity: 1, x: 0 }}
-        transition={{ ease: "easeInOut", delay: 1, duration: 2 }}
-      >
+      
         <div className="bg  flex-center flex-col space-y-8 p-5 w-full min-h-screen mx-auto ">
           <Benefits />
         </div>
-      </motion.div>
-      <motion.div
-        initial={{ opacity: 0, x: 300 }}
-        whileInView={{ opacity: 1, x: 0 }}
-        transition={{ ease: "easeInOut", delay: 1, duration: 2 }}
-      >
+     
         <div className="  max-w-[95%] pt-10 w-full mx-auto">
           <Support />
         </div>
-      </motion.div>
-      <motion.div
-        initial={{ opacity: 0, x: 300 }}
-        whileInView={{ opacity: 1, x: 0 }}
-        transition={{ ease: "easeInOut", delay: 1, duration: 2 }}
-      >
+    
         <div className="  max-w-[95%] pt-10 w-full mx-auto">
           <NewsRobot />
         </div>
-      </motion.div>
+    
     </div>
     </>
   );
