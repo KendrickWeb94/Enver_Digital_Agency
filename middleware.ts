de8a938f59a2a64e1b8bc50d1aec4536607e91b9
Kendrick Victor <sunflowerroses801@gmail.com>
2024-06-03 15:16:02 +0000

    Add files via upload

---
+++ middleware.ts
@@ -0,0 +1,10 @@
+import { authMiddleware } from "@clerk/nextjs/server";
+
+export default authMiddleware({
+  publicRoutes: ["/", "/contact"],
+});
+
+export const config = {
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
+
