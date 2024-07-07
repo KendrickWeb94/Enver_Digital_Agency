626c72176b2f310093b0eac9a86dedac415136c6
kendrickweb94 <kendrickzgeorge@gmail.com>
2024-07-07 14:28:21 -0700

    Updated Contact.tsx

--- app/Designs/Contact.tsx
+++ app/Designs/Contact.tsx
@@ -1,7 +1,9 @@
 /* eslint-disable react/no-unescaped-entities */
 import React from "react";
 import Image from "next/image";
-import contactphoto from "../assets/contact.png"
+import contactphoto from "../assets/contact.png";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
 
 const Contact = () => {
   return (
@@ -12,7 +14,7 @@ const Contact = () => {
       <div className=" absolute top-0  left-0 ">
         <div className=" bg-indigo-500 min-w-[150px] blur-[250px] max-w-[350px] h-[200px]"></div>
       </div>
-      <div className=" space-y-5 z-30">
+      <div className=" w-full space-y-5 z-30">
         <div className=" text-center z-30">
           {" "}
           <h1 className=" md:text-[48px] text-transparent bg-clip-text to-yellow-300  text-clip bg-gradient-to-r from-indigo-600 to leading-[3rem] sm:text-[36px] ds:text-[30px] text-balance font-extrabold">
@@ -23,12 +25,21 @@ const Contact = () => {
             illuminate our skies.
           </p>
         </div>
-        <div className=" max-w-2xl w-fit dark:border-[0.5px] shadow-xl dark:border-gray-500/35 rounded-md h-auto flex-wrap p-4 flex items-center justify-center">
-          <div className="contact-form">
-            <form></form>
-          </div>
-          <div className=" w-[300px] h-[500px] rounded-md overflow-hidden ">
-           <Image src={contactphoto} alt="" />
+        <div className="w-full flex items-center justify-center">
+          <div className=" max-w-2xl w-fit gap-4 dark:border-[0.5px] shadow-xl justify-center dark:border-gray-500/35 rounded-md h-auto flex-wrap p-4 flex items-center">
+            <div className="contact-form">
+              <form className=" w-[300px] space-y-5">
+                <Input placeholder="Enter your name" type="text" className=" w-full " />
+                <Input placeholder="Enter your email" type="email" className=" w-full " />
+                <Textarea placeholder="Type your message here"  className=" w-full " />
+                <button className="w-full p-2 rounded-md bg-indigo-600 hover:bg-gradient-to-r from-indigo-600 to-yellow-300 scroll-smooth flex items-center justify-center text-sm text-white">
+                  Rocket Your Message
+                </button>
+              </form>
+            </div>
+            <div className=" w-[300px] auto rounded-md overflow-hidden ">
+              <Image src={contactphoto} alt="" />
+            </div>
           </div>
         </div>
       </div>
