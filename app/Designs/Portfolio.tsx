3cc72884d9dbff350bbb3b0b6d7a5b21fee616d3
kendrickweb94 <kendrickzgeorge@gmail.com>
2024-07-15 11:08:33 -0700

    all

--- app/Designs/Portfolio.tsx
+++ app/Designs/Portfolio.tsx
@@ -1,4 +1,4 @@
-"use client";
+{/* "use client";
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination } from "swiper/modules";
@@ -76,3 +76,4 @@ const Portfolio: React.FC = () => {
 };
 
 export default Portfolio;
+*/}
